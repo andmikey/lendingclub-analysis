437f5f859d0f78af9229403dd06e09c1a66fcaf5
Mikey <31766607+andmikey@users.noreply.github.com>
2019-09-20 08:54:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
-    author='Your name (or your organization/company/team)',
+    description='Lending club data analysis',
+    author='Mikey (31766607+andmikey@users.noreply.github.com)',
     license='',
 )
